ce42a0be4e188bd06d9fee18dfaa4b2779f5a878
soieu <102034740+soieu@users.noreply.github.com>
2024-11-16 17:46:09 +0900

    네비게이션 기능 추가

--- src/app/(posts)/layout.tsx
+++ src/app/(posts)/layout.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
 export default function Post({ children }: { children: React.ReactNode }) {
+  const navRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    const navLinks: string[] = [];
+
+    headers.forEach((header, index) => {
+      const id = `section-${index}`;
+      header.setAttribute("id", id);
+      navLinks.push(`<li><a href="#${id}">${header.textContent}</a></li>`);
+    });
+
+    if (navRef.current) {
+      navRef.current.innerHTML = `<ul>${navLinks.join("")}</ul>`;
+    }
+  }, [children]);
+
   return (
     <div className="flex flex-row min-h-screen overflow-visible h-full">
-      <div className="w-5/6 mr-5 border border-black p-5">{children}</div>
-      <div className="sticky top-24 w-1/6 h-full border border-black p-5">
-        <div className="flex flex-wrap"></div>
+      <div className="w-4/5 mr-5 border border-black p-5">{children}</div>
+      <div className="sticky top-24 w-1/5 h-full border border-black p-5">
+        <div className="flex flex-wrap" ref={navRef}></div>
       </div>
     </div>
   );
--- src/components/post-column.tsx
+++ src/components/post-column.tsx
@@ -12,6 +12,7 @@ export default function PostColumn({ post }: { post: Post }) {
     <Link href={`/${post.slug}`}>
       <div className="border border-black w-full hover:bg-black hover:text-white hover:cursor-pointer mb-5">
         <div className="text-5xl font-bold p-5">{post.title}</div>
+
         <div className="text-base text-gray-500 text-right px-5 py-2">
           {new Intl.DateTimeFormat("ko-KR", {
             year: "numeric",
