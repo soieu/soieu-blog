63878b55f6dcd28b6a0849363240d7d18af43df2
soieu <102034740+soieu@users.noreply.github.com>
2024-11-20 13:51:25 +0900

    태그 스타일링 수정!!

--- src/components/tag-tab.tsx
+++ src/components/tag-tab.tsx
@@ -7,7 +7,7 @@ export default async function TagTab({
   params: { tag: string } | null;
 }) {
   return (
-    <div className="xl:w-1/4 border border-black p-5 bg-white mb-3 flex flex-wrap flex-row">
+    <div className="xl:w-1/4 border border-black p-5 bg-white mb-3 flex flex-wrap flex-row xl:h-full">
       {getTagsWithCount().then((tags) =>
         tags.map((tag) => <Tag tag={tag} key={tag.tag} params={params} />)
       )}
