7fc2f9c519d21c0aca268156f5bf69c14e728de8
soieu <102034740+soieu@users.noreply.github.com>
2024-11-20 13:48:13 +0900

    코드블럭 스타일링 & 글 업데이트

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 import rehypeCodeTitles from "rehype-code-titles";
-
+import rehypeHighlight from "rehype-highlight";
+import rehypePrism from "rehype-prism-plus";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  transpilePackages: ["next-mdx-remote"],
   reactStrictMode: true,
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
@@ -17,7 +19,7 @@ const withMDX = nextMDX({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeCodeTitles],
+    rehypePlugins: [rehypeCodeTitles, rehypeHighlight, rehypePrism],
   },
 });
 
